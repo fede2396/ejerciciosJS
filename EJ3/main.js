73c8c2feeac9f864da881a9bd571fd0069c1d7b4
fede2396 <fede.acosta2396@gmail.com>
2022-07-13 13:04:42 -0300

    v1

---
+++ EJ3/main.js
@@ -0,0 +1,39 @@
+function Pizza (id, ingredientes , precio ,nombre) {
+    this.id = id;
+    this.nombre = nombre;
+    this.ingredientes = ingredientes;
+    this.precio = precio;
+    
+}
+
+
+let fugazza = new Pizza('Fugazza', ['Queso, Cebolla'], 790,1);
+let muzzarella = new Pizza('Muzarella', ['Queso', 'Salsa'], 590,2);
+let napolitana = new Pizza('Napolitana', ['Queso', 'Tomate', 'Albahaca','Salsa'], 660,3);
+let hawaiana = new Pizza('Hawaiana', ['Queso', 'Ananá'], 850, 4);
+let pepperoni = new Pizza('Pepperoni', ['Queso', 'Peperoni', 'Salsa'], 800, 5);
+
+let pizzas = [fugazza, muzzarella , napolitana ,hawaiana ,pepperoni ];
+
+let pizzaFound = document.querySelector('.pizza-search');
+
+let pizzaPrice = document.querySelector('.pizza-price');
+
+let searchBtn = document.getElementById('search-btn');
+
+searchBtn.addEventListener('click', searchPizzaId);
+
+function searchPizzaId (){
+    let getId = document.getElementById('pizza-id').value;
+    const searchPizza = pizzas.find(({id}) => id == getId);
+
+    if(!searchPizza){
+        pizzaFound.innerHTML = 'Pizza no encontrada';
+        pizzaPrice.innerHTML = '';
+    }else{
+        console.log(searchPizza.id);
+        pizzaFound.innerHTML = searchPizza.nombre;
+        pizzaPrice.innerHTML = `$${searchPizza.precio}`
+
+    }
+}
